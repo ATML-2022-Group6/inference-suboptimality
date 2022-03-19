301f99e3e05b9ab69ec2b741ba91a1eb5dccce6b
basimkhajwal <basimkhajwal@gmail.com>
2022-03-19 10:45:59 +0000

    Go back to original logpx

--- vae.py
+++ vae.py
@@ -17,8 +17,15 @@ class HyperParams:
   act_fun: tuple = stax.Elu
 
 def log_bernoulli(logit, target):
-  # TODO: Use BCE
-  return -jnp.sum(jnp.logaddexp(0., (1 - target * 2) * logit))
+  # Numerically equivalent to Binary Cross Entropy loss (TODO: How?)
+  bce_loss = jnp.maximum(logit, 0.) - logit * target + jnp.logaddexp(0., -jnp.abs(logit))
+  return -jnp.sum(bce_loss)
+
+# Equivalent to below
+# def log_sigmoid(x):
+#   return -jnp.logaddexp(0, -x)
+# def bce(logit, target):
+#   return jnp.sum( target * log_sigmoid(logit) + (1 - target) * log_sigmoid(-logit) )
 
 def gaussian_kld(mu, logvar):
   return -0.5 * jnp.sum(1. + logvar - mu**2. - jnp.exp(logvar))
