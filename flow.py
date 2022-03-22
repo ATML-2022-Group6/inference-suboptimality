9275783b9097ee81f45f6a17e9775f129b434812
Salman <salmanfaris98@gmail.com>
2022-03-22 14:29:12 +0000

    Completed implementation of auxiliary flow

--- flow.py
+++ flow.py
@@ -17,33 +17,51 @@ def build_aux_flow(hps: HyperParams):
   # indexed as 1:d, d+1:D.
   latent_split_size: int = latent_size // 2
 
-  h1_net_init, h1_net_apply = stax.serial(stax.Dense(hidden_size), stax.Tanh)
-  h2_net_init, h2_net_apply = stax.serial(stax.Dense(hidden_size), stax.Tanh)
-  μ1_net_init, μ1_net_apply = stax.Dense(latent_split_size)
-  μ2_net_init, μ2_net_apply = stax.Dense(latent_split_size)
-  σ1_net_init, σ1_net_apply = stax.Dense(latent_split_size)
-  σ2_net_init, σ2_net_apply = stax.Dense(latent_split_size)
+  # Nets for `_norm_flow()` method; eq. (9) and (10) in Cremer et al.
+  h1_net_init, h1_net = stax.serial(stax.Dense(hidden_size), stax.Tanh)
+  h2_net_init, h2_net = stax.serial(stax.Dense(hidden_size), stax.Tanh)
+  μ1_net_init, μ1_net = stax.Dense(latent_split_size)
+  μ2_net_init, μ2_net = stax.Dense(latent_split_size)
+  σ1_net_init, σ1_net = stax.Dense(latent_split_size)
+  σ2_net_init, σ2_net = stax.Dense(latent_split_size)
+  
+  # Nets for `sample()` method.
+  rv_net_init, rv_net = stax.serial(
+    stax.Dense(hidden_size), stax.Elu,
+    stax.Dense(hidden_size), stax.Elu,
+    stax.Dense(2*latent_size),  # Twice the size so we can split into mean & logvar resp.
+  )
   
   def init_fun(rng):
-    rngs = random.split(rng, num=6)
+    rngs = random.split(rng, num=7)
 
     h1_size, h1_net_params = h1_net_init(rngs[0], input_shape=(latent_split_size,))
-    μ1_size, μ1_net_params = μ1_net_init(rngs[1], input_shape=h1_size)
-    σ1_size, σ1_net_params = σ1_net_init(rngs[2], input_shape=h1_size)
+    _, μ1_net_params = μ1_net_init(rngs[1], input_shape=h1_size)
+    _, σ1_net_params = σ1_net_init(rngs[2], input_shape=h1_size)
 
     h2_size, h2_net_params = h2_net_init(rngs[3], input_shape=(latent_split_size,))
-    μ2_size, μ2_net_params = μ2_net_init(rngs[4], input_shape=h2_size)
-    σ2_size, σ2_net_params = σ2_net_init(rngs[5], input_shape=h2_size)
+    _, μ2_net_params = μ2_net_init(rngs[4], input_shape=h2_size)
+    _, σ2_net_params = σ2_net_init(rngs[5], input_shape=h2_size)
+
+    _, rv_net_params = rv_net_init(6, input_shape=(2*latent_size,))
 
     params = (
-      h1_net_params, μ1_net_params, σ1_net_params,
-      h2_net_params, μ2_net_params, σ2_net_params
+      (
+        h1_net_params, 
+        μ1_net_params, 
+        σ1_net_params,
+        h2_net_params, 
+        μ2_net_params, 
+        σ2_net_params,
+      ),
+      rv_net_params,
     )
 
     return params
   
   def sample(rng, z0, params):
     """The forward function essentially."""
+    norm_flow_params, aux_var_params = params
     
     # Auxiliary variable: q(v0)
     mu, logvar = jnp.zeros(latent_size), jnp.zeros(latent_size)
@@ -56,41 +74,49 @@ def build_aux_flow(hps: HyperParams):
     # TODO :- For loop on the number of flows (has to have as many 
     # neural nets). For this implementation may need to bring current net inits
     # `_norm_flow()` to parent function init.
-    zT, vT, logdet = _norm_flow(params, z0, v0)
+    zT, vT, logdet = _norm_flow(norm_flow_params, z0, v0)
     logdetsum += logdet
     
     # Reverse distribution: r(vT|x,zT)
-    rv_weights: any
-    out = zT
-    for i in range(len(rv_weights)-1):
-      out = nn.elu(rv_weights[i](out))
-    out = rv_weights[-1](out)
-    mean_vT, logvar_vT = out[:latent_size], out[latent_size:]  # TODO :- this depends on our net architecture.
-    logrvT = log_normal(vT, mean_vT, logvar_vT)
+    logrvT = _aux_var(aux_var_params, zT, vT)
     
     logprob = logqv0 - logdetsum - logrvT
-    
     return zT, logprob
   
+  def _aux_var(params, zT, vT):
+    """Auxiliary variable procedure."""
+    rv_net_params = params
+    
+    out = rv_net(rv_net_params, zT)
+    mean_vT, logvar_vT = out[:latent_size], out[latent_size:]
+    logrvT = log_normal(vT, mean_vT, logvar_vT)
+    
+    return logrvT
+  
   def _norm_flow(params, z, v):
-    """Normalizing flow implementation without Auxiliary Variable.
-    Based on equation (9) and (10) in our paper Cremer et al."""
-
+    """
+    Normalizing flow as defined by equation (9) and (10) in our 
+    paper Cremer et al.
+    """
     (
-      h1_net_params, μ1_net_params, σ1_net_params,
-      h2_net_params, μ2_net_params, σ2_net_params
+      h1_net_params, 
+      μ1_net_params, 
+      σ1_net_params,
+      h2_net_params, 
+      μ2_net_params, 
+      σ2_net_params,
     ) = params
     
     # Equation (9) in paper.
-    h1 = h1_net_apply(h1_net_params, z)
-    μ1 = μ1_net_apply(μ1_net_params, h1)
-    logit_1 = σ1_net_apply(σ1_net_params, h1)
+    h1 = h1_net(h1_net_params, z)
+    μ1 = μ1_net(μ1_net_params, h1)
+    logit_1 = σ1_net(σ1_net_params, h1)
     v = v*nn.sigmoid(logit_1) + μ1  # TODO :- Add nn.elu to μ1 and multiply with `grad_fn(z)`.
 
     # Equation (10) in paper.
-    h2 = h2_net_apply(h2_net_params, v)
-    μ2 = μ2_net_apply(μ2_net_params, h2)
-    logit_2 = σ2_net_apply(σ2_net_params, h2)
+    h2 = h2_net(h2_net_params, v)
+    μ2 = μ2_net(μ2_net_params, h2)
+    logit_2 = σ2_net(σ2_net_params, h2)
     z = z*nn.sigmoid(logit_2) + μ2  # TODO :- Add nn.elu to μ2 and multiply with `grad_fn(z)`.
 
     logdet_v = jnp.sum(logit_1 - nn.softplus(logit_1))
@@ -122,12 +148,12 @@ def build_flow(hps: HyperParams):
   #    u1   o1     u2    o2
   #       
 
-  h1_net_init, h1_net_apply = stax.serial(stax.Dense(hidden_size), stax.Tanh)
-  h2_net_init, h2_net_apply = stax.serial(stax.Dense(hidden_size), stax.Tanh)
-  μ1_net_init, μ1_net_apply = stax.Dense(latent_split_size)
-  μ2_net_init, μ2_net_apply = stax.Dense(latent_split_size)
-  σ1_net_init, σ1_net_apply = stax.Dense(latent_split_size)
-  σ2_net_init, σ2_net_apply = stax.Dense(latent_split_size)
+  h1_net_init, h1_net = stax.serial(stax.Dense(hidden_size), stax.Tanh)
+  h2_net_init, h2_net = stax.serial(stax.Dense(hidden_size), stax.Tanh)
+  μ1_net_init, μ1_net = stax.Dense(latent_split_size)
+  μ2_net_init, μ2_net = stax.Dense(latent_split_size)
+  σ1_net_init, σ1_net = stax.Dense(latent_split_size)
+  σ2_net_init, σ2_net = stax.Dense(latent_split_size)
   
   def init_fun(rng):
     rngs = random.split(rng, num=6)
@@ -177,15 +203,15 @@ def build_flow(hps: HyperParams):
     ) = params
     
     # Equation (9) in paper.
-    h1 = h1_net_apply(h1_net_params, z)
-    μ1 = μ1_net_apply(μ1_net_params, h1)
-    logit_1 = σ1_net_apply(σ1_net_params, h1)
+    h1 = h1_net(h1_net_params, z)
+    μ1 = μ1_net(μ1_net_params, h1)
+    logit_1 = σ1_net(σ1_net_params, h1)
     v = v*nn.sigmoid(logit_1) + μ1  # TODO :- Add nn.elu to μ1 and multiply with `grad_fn(z)`.
 
     # Equation (10) in paper.
-    h2 = h2_net_apply(h2_net_params, v)
-    μ2 = μ2_net_apply(μ2_net_params, h2)
-    logit_2 = σ2_net_apply(σ2_net_params, h2)
+    h2 = h2_net(h2_net_params, v)
+    μ2 = μ2_net(μ2_net_params, h2)
+    logit_2 = σ2_net(σ2_net_params, h2)
     z = z*nn.sigmoid(logit_2) + μ2  # TODO :- Add nn.elu to μ2 and multiply with `grad_fn(z)`.
 
     logdet_v = jnp.sum(logit_1 - nn.softplus(logit_1))
