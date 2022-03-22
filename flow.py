768ec10776937765dc86c49e5ff09e754e18be86
Salman <salmanfaris98@gmail.com>
2022-03-22 17:00:22 +0000

    Reduced flow architecture using stax parallel

--- flow.py
+++ flow.py
@@ -12,12 +12,22 @@ def build_aux_flow(hps: HyperParams):
   latent_size: int = hps.latent_size
 
   # Nets for `_norm_flow()` method; eq. (9) and (10) in Cremer et al.
-  h1_net_init, h1_net = stax.serial(stax.Dense(hidden_size), stax.Elu)
-  h2_net_init, h2_net = stax.serial(stax.Dense(hidden_size), stax.Elu)
-  μ1_net_init, μ1_net = stax.Dense(latent_size)
-  μ2_net_init, μ2_net = stax.Dense(latent_size)
-  σ1_net_init, σ1_net = stax.Dense(latent_size)
-  σ2_net_init, σ2_net = stax.Dense(latent_size)
+  net1_init, net1 = stax.serial(
+    stax.Dense(hidden_size), stax.Elu, # --> h
+    stax.FanOut(2),
+    stax.parallel(
+      stax.Dense(latent_size), # ---> μ
+      stax.Dense(latent_size), # ---> logit
+    ),
+  )
+  net2_init, net2 = stax.serial(
+    stax.Dense(hidden_size), stax.Elu, # --> h 
+    stax.FanOut(2),
+    stax.parallel(
+      stax.Dense(latent_size), # ---> μ
+      stax.Dense(latent_size), # ---> logit
+    ),
+  )
   
   # Nets for `sample()` method.
   rv_net_init, rv_net = stax.serial(
@@ -27,26 +37,16 @@ def build_aux_flow(hps: HyperParams):
   )
   
   def init_fun(rng):
-    rngs = random.split(rng, num=7)
+    rngs = random.split(rng, num=3)
 
-    h1_size, h1_net_params = h1_net_init(rngs[0], input_shape=(latent_size,))
-    _, μ1_net_params = μ1_net_init(rngs[1], input_shape=h1_size)
-    _, σ1_net_params = σ1_net_init(rngs[2], input_shape=h1_size)
-
-    h2_size, h2_net_params = h2_net_init(rngs[3], input_shape=(latent_size,))
-    _, μ2_net_params = μ2_net_init(rngs[4], input_shape=h2_size)
-    _, σ2_net_params = σ2_net_init(rngs[5], input_shape=h2_size)
-
-    _, rv_net_params = rv_net_init(6, input_shape=(2*latent_size,))
+    _, net1_params = net1_init(rngs[0], input_shape=(latent_size,))
+    _, net2_params = net2_init(rngs[1], input_shape=(latent_size,))
+    _, rv_net_params = rv_net_init(rngs[2], input_shape=(2*latent_size,))
 
     params = (
       (
-        h1_net_params, 
-        μ1_net_params, 
-        σ1_net_params,
-        h2_net_params, 
-        μ2_net_params, 
-        σ2_net_params,
+        net1_params,
+        net2_params,
       ),
       rv_net_params,
     )
@@ -86,12 +86,8 @@ def build_aux_flow(hps: HyperParams):
     by equation (9) and (10) in our paper Cremer et al.
     """
     (
-      h1_net_params, 
-      μ1_net_params, 
-      σ1_net_params,
-      h2_net_params, 
-      μ2_net_params, 
-      σ2_net_params,
+      net1_params,
+      net2_params,
     ) = params
     
     # Our flow's affine coupling procedure (modulo indexing).
@@ -114,16 +110,14 @@ def build_aux_flow(hps: HyperParams):
     #
     
     # Equation (9) in paper. No difference doing z_1 or z_2 first in the coupling.
-    h1 = h1_net(h1_net_params, z)
-    μ1 = μ1_net(μ1_net_params, h1)
-    logit_1 = σ1_net(σ1_net_params, h1)
-    v = v*nn.sigmoid(logit_1) + μ1  # TODO :- Add nn.elu to μ1 and multiply with `grad_fn(z)`.
+    μ1, logit_1 = net1(net1_params, z)
+    σ1 = nn.sigmoid(logit_1)
+    v = v*σ1 + μ1 
 
     # Equation (10) in paper. No difference doing z_1 or z_2 first in the coupling.
-    h2 = h2_net(h2_net_params, v)
-    μ2 = μ2_net(μ2_net_params, h2)
-    logit_2 = σ2_net(σ2_net_params, h2)
-    z = z*nn.sigmoid(logit_2) + μ2  # TODO :- Add nn.elu to μ2 and multiply with `grad_fn(z)`.
+    μ2, logit_2 = net2(net2_params, v)
+    σ2 = nn.sigmoid(logit_2)
+    z = z*σ2 + μ2 
 
     logdet_v = jnp.sum(logit_1 - nn.softplus(logit_1))
     logdet_z = jnp.sum(logit_2 - nn.softplus(logit_2))
