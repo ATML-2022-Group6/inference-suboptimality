bcffe8a2502be52356bf89517be3b51a0a3f8cd2
Salman <salmanfaris98@gmail.com>
2022-03-23 22:13:40 +0000

    Refactor code for easy HMC sampling. Also made tuning output more explicit.

--- hmc.py
+++ hmc.py
@@ -1,12 +1,71 @@
 from jax import numpy as jnp
 from jax import random
 
-def hmc_accept_reject(
+def hmc_sample_and_tune(
+    rng,
+    current_q,
+    current_p,
+    grad_U,
+    U, K,
+    tuning_params,
+):  
+    """Our HMC magic function."""
+    # Hamiltonian dynamics to propose new (q*, p*).
+    proposed_q, proposed_p = _leapfrog_integrator(current_q, current_p, grad_U, stepsize)
+
+    # Accept-reject procedure on (q*, p*) & adaptive tuning.
+    current_q, stepsize, accept_trace = _hmc_accept_reject_adapt(
+        rng,
+        current_q, current_p,
+        proposed_q, proposed_p, 
+        U, K,
+        tuning_params
+    )
+    tuning_params_no_period = (stepsize, accept_trace)
+    
+    return current_q, tuning_params_no_period
+
+def _leapfrog_integrator(
+    current_q,
+    # Initial momentum is always initialized as iid N(0, 1).
+    # Only reason we need is because of the two-part implementation.
+    current_p, 
+    grad_U, 
+    stepsize, num_steps=10
+):
+    """
+    Based on Xuechen, which in turn is based on 
+    Neal (https://arxiv.org/pdf/1206.1901.pdf).
+    """
+    # For some reason, without the reshape things break.
+    stepsize = jnp.reshape(stepsize, newshape=(-1, 1))  
+    
+    q = current_q
+    
+    # Start with half step for momentum.
+    p = current_p - 0.5*stepsize*grad_U(q)
+    
+    for i in range(1, num_steps+1):
+        q = q + p*stepsize  # Full step for position.
+        # Full step for momentum, while not end of trajectory.
+        if i < num_steps:
+            p = p - stepsize*grad_U(q)
+    
+    # Finally do a half step for momentum.
+    p = p - 0.5*stepsize*grad_U(q)
+    
+    # To make proposal symmetric. Not actually needed since K(p) = K(-p),
+    # and p will be replaced in the next iteration.
+    p = -p
+    
+    return q, p
+
+def _hmc_accept_reject_adapt(
     rng,
     current_q, current_p,
-    q, p, epsilon,
-    accept_trace, trace_len,
-    U, K
+    q, p,
+    U, K,
+    tuning_params,
 ):
     """
     Hamiltonian Monte Carlo accept-reject procedure
@@ -23,46 +82,16 @@ def hmc_accept_reject(
     q = q*accepts + current_q*(1.-accepts)
     
     # HMC tuning procedure.
+    (
+      stepsize, 
+      accept_trace, 
+      trace_period,
+    ) = tuning_params
     accept_trace = accept_trace + accepts
     optimal_acceptance_rate = 0.65  # cf pg. 29 in https://arxiv.org/pdf/1206.1901.pdf
-    criteria = accept_trace/trace_len > optimal_acceptance_rate
+    
+    criteria = accept_trace/trace_period > optimal_acceptance_rate
     adapt = 1.02*criteria + 0.98*(1. - criteria)
-    epsilon = epsilon * adapt  # Missing clamp onto [1e-4, .5] as in XC; maybe needed in practice?
+    stepsize = stepsize * adapt  # Missing clamp onto [1e-4, .5] as in XC; maybe needed in practice?
     
-    return q, epsilon, accept_trace
-
-def leapfrog_integrator(
-    current_q,
-    current_p, 
-    U, grad_U, 
-    epsilon, num_steps=10
-):
-    """
-    Based on Xuechen, which in turn is based on 
-    Neal (https://arxiv.org/pdf/1206.1901.pdf).
-    """
-    # For some reason, without the reshape things break.
-    epsilon = jnp.reshape(epsilon, newshape=(-1, 1))  
-    
-    # Initial momentum is always initialized as iid N(0, 1).
-    # Only reason we need is because of the two-part implementation.
-    q = current_q
-    
-    # Start with half step for momentum.
-    p = current_p - 0.5*epsilon*grad_U(q)
-    
-    for i in range(1, num_steps+1):
-        q = q + p*epsilon  # Full step for position.
-        
-        # Full step for momentum, while not end of trajectory.
-        if i < num_steps:
-            p = p - epsilon*grad_U(q)
-    
-    # Finally do a half step for momentum.
-    p = p - 0.5*epsilon*grad_U(q)
-    
-    # To make proposal symmetric. Not actually needed since K(p) = K(-p),
-    # and p will be replaced in the next iteration.
-    p = -p
-    
-    return q, p
+    return q, stepsize, accept_trace
