696da52128f72e8129ae74a99856b192806f9e65
Snehal Raj <snehalraj808@gmail.com>
2022-03-23 11:20:43 +0000

    local ffg upate

--- local_ffg.py
+++ local_ffg.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from jax.scipy.special import logsumexp
 import time
 from jax.scipy import stats
-
+from jax import numpy as jnp
 from jax.example_libraries import optimizers
 from tqdm.notebook import tqdm, trange
 from jax import jit
@@ -15,26 +15,28 @@ num_samples = 32
 
 opt_init, opt_update, get_params = optimizers.adam(step_size=1e-3, eps=1e-4)
 
-def loss_fn(rng, params, image):
-  elbo, _, _, _= run_vae(params, image, rng=rng)
+def loss_fn(rng, enc_params, decoder_params, image):
+  mu, logvar = enc_params
+  elbo, _, _, _= run_vae_local(rng, image, mu, logvar, decoder_params)
   return -elbo
 
-def iwelbo_fn(rng, params, image):
+def iwelbo_fn(rng, enc_params, decoder_params, image):
     rngs = random.split(rng, num_samples)
-    iw_log_summand, _, _, _ = jax.vmap(run_vae, in_axes=(None, None, 0))( params, image, rngs)
+    mu, logvar = enc_params
+    iw_log_summand, _, _, _ = jax.vmap(run_vae_local, in_axes=(0, None, None, None, None))( rngs, image, mu, logvar, decoder_params )
 
     K = num_samples
     iwelbo_K = logsumexp(iw_log_summand) - jnp.log(K)
     return iwelbo_K
 
-def batch_iwelbo_fn(rng, params, images):
+def batch_iwelbo_fn(rng, enc_params, decoder_params, images):
   rngs = random.split(rng, batch_size)
-  return jnp.mean(jax.vmap(iwelbo_fn, in_axes=(0, None, 0))(rngs, params, images))
+  return jnp.mean(jax.vmap(iwelbo_fn, in_axes=(0, 0, None, 0))(rngs, enc_params, decoder_params, images))
 
 
-def batch_loss_fn(rng, params, images):
+def batch_loss_fn(rng, enc_params, decoder_params, images):
   rngs = random.split(rng, batch_size)
-  return jnp.mean(jax.vmap(loss_fn, in_axes=(0, None, 0))(rngs, params, images))
+  return jnp.mean(jax.vmap(loss_fn, in_axes=(0, 0, None, 0))(rngs, enc_params, decoder_params, images))
 
 @jit
 def run_iwelbo_epoch(epoch, rng,opt_state, decoder_params, batch):
@@ -42,7 +44,7 @@ def run_iwelbo_epoch(epoch, rng,opt_state, decoder_params, batch):
   def body_fn(opt_state, args):
     idx, rng, batch = args
     enc_params = get_params(opt_state)
-    loss = batch_iwelbo_fn(rng, (enc_params,decoder_params), batch)
+    loss = batch_iwelbo_fn(rng, enc_params,decoder_params, batch)
     return loss
 
   scan_args = (epoch, rng, batch)
@@ -50,16 +52,13 @@ def run_iwelbo_epoch(epoch, rng,opt_state, decoder_params, batch):
 
   return losses
 
-def another_fn(rng, encoder_params, decoder_params, batch):
-  return batch_loss_fn(rng, (encoder_params, decoder_params), batch)
-
 @jit
 def run_epoch(epoch, rng, opt_state, decoder_params, batch):
 
   def body_fn(opt_state, args):
     idx, rng, batch = args
     enc_params = get_params(opt_state)
-    loss, g = jax.value_and_grad(another_fn, argnums=1)(rng, enc_params, decoder_params, batch)
+    loss, g = jax.value_and_grad(batch_loss_fn, argnums=1)(rng, enc_params, decoder_params, batch)
     return opt_update(idx, g, opt_state), loss
 
   scan_args = (epoch, rng, batch)
@@ -77,13 +76,18 @@ def optimize_local_gaussian(
     log_likelihood,
     decoder_params,
     batch,
+    z_size,
     check_every=100,
     sentinel_thres=10,
     debug=False
 ):
-    init_rng = random.PRNGKey(0)
-    init_encoder_params, _ = init_vae(rng=init_rng, input_shape=(28 * 28,))
-    init_params = init_encoder_params
+    # init_rng = random.PRNGKey(0)
+    # init_encoder_params, _ = init_vae(rng=init_rng, input_shape=(28 * 28,))
+    batch_size = len(batch)
+    latent_size = z_size
+    mu0 = jnp.zeros((batch_size,latent_size))
+    logvar0 = jnp.zeros((batch_size, latent_size))
+    init_params = (mu0, logvar0)
 
     opt_state = opt_init(init_params)
     rng = random.PRNGKey(0)
@@ -136,7 +140,7 @@ def local_FFG(params, z_size, batches):
     time_ = time.time()
     prev_seq = []
     for i, batch in enumerate(tqdm(batches)):
-        elbo, iwae, _ , _ = optimize_local_gaussian(log_bernoulli, decoder_params, batch)
+        elbo, iwae, _ , _ = optimize_local_gaussian(log_bernoulli, decoder_params, batch, z_size)
         vae_record.append(elbo)
         iwae_record.append(iwae)
         print ('Local opt w/ ffg, batch %d, time elapse %.4f, ELBO %.4f, IWAE %.4f' % \
@@ -150,5 +154,6 @@ def local_FFG(params, z_size, batches):
     
 
 
-# #To run: 
+
+#To run: 
 # local_FFG(get_params(opt_state), 50, train_batches)
