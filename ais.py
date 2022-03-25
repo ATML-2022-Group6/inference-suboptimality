dc1d64ba02f1b845d62ab0151c914fb60e576602
Salman <salmanfaris98@gmail.com>
2022-03-25 15:41:55 +0000

    AIS added return importance weight

--- ais.py
+++ ais.py
@@ -16,31 +16,31 @@ def ais_trajectory(
 ):
   """Annealed importance sampling trajectories for a single batch."""
   latent_size = hps.latent_size
-  
+
   v_rng, z_rng, hmc_rng = random.split(rng, num=3)
 
   def _intermediate_dist(z, x, beta, log_likelihood_fn=log_bernoulli):
       zeros = jnp.zeros(z.shape)
       log_prior = log_normal(z, zeros, zeros)
-      
+
       logit = decoder(decoder_params, z)
       log_likelihood = log_likelihood_fn(logit, x)
 
       return log_prior + (beta*log_likelihood)
-    
+
   stepsize = jnp.ones(1)*0.01
   accept_trace = jnp.zeros(1)
   log_importance_weight = jnp.zeros(1)  # XC has volatile=True in torch tensor
-  
+
   # No need backward implementation, since not doing BDMC!
   current_z = random.normal(z_rng, shape=(latent_size,))
 
   log_f_i = _intermediate_dist
 
   # WARNING :- enumerate has to start with 1 due to division by period_elapsed in HMC.
-  for period_elapsed, (beta_0, beta_1) in enumerate(zip(annealing_schedule[:-1], 
+  log_importance_weight = jnp.zeros(current_z.shape)
+  for period_elapsed, (beta_0, beta_1) in enumerate(zip(annealing_schedule[:-1],
                                                         annealing_schedule[1:]), 1):
-
     # Log importance weight update
     log_int_1 = log_f_i(current_z, x, beta_0)
     log_int_2 = log_f_i(current_z, x, beta_1)
@@ -51,7 +51,7 @@ def ais_trajectory(
 
     def grad_U(z):
       gradient = grad(U)(z)
-      gradient = lax.clamp(-10000., gradient, 10000.) 
+      gradient = lax.clamp(-10000., gradient, 10000.)
       return gradient
 
     def normalized_K(v):
@@ -61,8 +61,10 @@ def ais_trajectory(
     tuning_params = (stepsize, accept_trace, period_elapsed)
     current_v = random.normal(v_rng, shape=current_z.shape)
     current_z, stepsize, accept_trace = hmc_sample_and_tune(
-        hmc_rng, 
+        hmc_rng,
         current_z, current_v,
         U, normalized_K, grad_U,
         tuning_params
     )
+  
+  return log_importance_weight
