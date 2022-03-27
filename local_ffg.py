7bab6c76d499e188d39942e1780d33a123498826
Vasileios Ntogram <vasnto@gmail.com>
2022-03-27 11:56:42 +0100

    Update local_ffg.py

--- local_ffg.py
+++ local_ffg.py
@@ -139,7 +139,7 @@ def optimize_local_gaussian(
     # evaluation
     vae_elbo = -loss
     iwae_elbo = iw_loss
-    return vae_elbo, iwae_elbo
+    return vae_elbo, iwae_elbo, plot_elbo, plot_iwae
 
 def local_FFG(params, z_size, batches):
     _, decoder_params = params
@@ -147,7 +147,7 @@ def local_FFG(params, z_size, batches):
     time_ = time.time()
     prev_seq = []
     for i, batch in enumerate(tqdm(batches)):
-        elbo, iwae = optimize_local_gaussian(log_bernoulli, decoder_params, batch, z_size)
+        elbo, iwae, _, _ = optimize_local_gaussian(log_bernoulli, decoder_params, batch, z_size)
         vae_record.append(elbo)
         iwae_record.append(iwae)
 
