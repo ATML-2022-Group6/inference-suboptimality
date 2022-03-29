61917f009be0f38af4ad06a93efc158139a214fe
Salman <salmanfaris98@gmail.com>
2022-03-29 22:49:16 +0100

    Introduced x into the aux flow implementation

--- flow2.py
+++ flow2.py
@@ -9,6 +9,7 @@ def build_aux_flow(hps: HyperParams):
   """Normalizing flow + auxiliary variable."""
   hidden_size: int = 50
   latent_size: int = hps.latent_size
+  image_size: int = hps.image_size
 
   # Nets for `_norm_flow()` method; eq. (9) and (10) in Cremer et al.
   # We assume that each net for each flow assumes the same architecture.
@@ -38,14 +39,14 @@ def build_aux_flow(hps: HyperParams):
     rngs = random.split(rng, num=6)
 
     # Flow procedure.
-    _, flow1_net2_params = flow_net_init(rngs[1], input_shape=(latent_size,))
-    _, flow1_net1_params = flow_net_init(rngs[0], input_shape=(latent_size,))
-    _, flow2_net1_params = flow_net_init(rngs[2], input_shape=(latent_size,))
-    _, flow2_net2_params = flow_net_init(rngs[3], input_shape=(latent_size,))
+    _, flow1_net1_params = flow_net_init(rngs[0], input_shape=(image_size+latent_size,))
+    _, flow1_net2_params = flow_net_init(rngs[1], input_shape=(image_size+latent_size,))
+    _, flow2_net1_params = flow_net_init(rngs[2], input_shape=(image_size+latent_size,))
+    _, flow2_net2_params = flow_net_init(rngs[3], input_shape=(image_size+latent_size,))
     
     # Auxiliary variable procedure.
-    _, qv_net_params = model_net_init(rngs[4], input_shape=(latent_size,))
-    _, rv_net_params = model_net_init(rngs[5], input_shape=(latent_size,))
+    _, qv_net_params = model_net_init(rngs[4], input_shape=(image_size+latent_size,))
+    _, rv_net_params = model_net_init(rngs[5], input_shape=(image_size+latent_size,))
 
     params = (
       (
@@ -60,7 +61,7 @@ def build_aux_flow(hps: HyperParams):
 
     return params
   
-  def sample(rng, z0, params):
+  def sample(rng, z0, x, params):
     """The forward function essentially."""
     norm_flow_params, aux_var_params = params
     (
@@ -68,12 +69,10 @@ def build_aux_flow(hps: HyperParams):
       rv_net_params,
     ) = aux_var_params
     
-    # Auxiliary variable, forward distribution: q(v0)
-    # IMPLEMENTATION CONFLICTS :- Cremer initialized mu, logvar with zeros
-    # whereas Xuechen get mu, logvar from net transformations on z0.
-    #
-    # mean_v0, logvar_v0 = qv_net(qv_net_params, z0)  # Xuechen's method
-    mean_v0, logvar_v0 = jnp.zeros(latent_size), jnp.zeros(latent_size)  # Cremer's method
+    zx = jnp.concatenate((z0, x), axis=1)
+    
+    # Auxiliary variable, forward model q(v|x,z).
+    mean_v0, logvar_v0 = model_net(qv_net_params, zx)
     eps = random.normal(rng, mean_v0.shape)
     v0 = mean_v0 + eps*jnp.exp(0.5 * logvar_v0)
     logqv0 = log_normal(v0, mean_v0, logvar_v0)
@@ -85,15 +84,16 @@ def build_aux_flow(hps: HyperParams):
                                       norm_flow_params[1])
     inverse_logdet_sum = -(logdet_flow1 + logdet_flow2)
     
-    # Reverse distribution: r(vT|x,zT).
-    logrvT = _reverse_aux_var(zT, vT, rv_net_params)
+    # Reverse model: r(v|x,z).
+    zx = jnp.concatenate((zT, x), axis=1)
+    logrvT = _reverse_aux_var(zx, vT, rv_net_params)
     
     # Auxiliary flow correction to log q(z|x).
     logprob = logqv0 + inverse_logdet_sum - logrvT
     
     return zT, logprob
   
-  def _norm_flow(z, v, params):
+  def _norm_flow(z, v, x, params):
     """
     Real-NVP (Dinh et al.) normalizing flow as defined 
     by equation (9) and (10) in our paper Cremer et al.
@@ -120,12 +120,12 @@ def build_aux_flow(hps: HyperParams):
     #
     
     # Equation (9) in paper. No difference doing z_1 or z_2 first in the coupling.
-    μ1, logit_1 = flow_net(net1_params, z)
+    μ1, logit_1 = flow_net(net1_params, jnp.concatenate((z, x), axis=1))
     σ1 = nn.sigmoid(logit_1)
     v = v*σ1 + μ1 
 
     # Equation (10) in paper. No difference doing z_1 or z_2 first in the coupling.
-    μ2, logit_2 = flow_net(net2_params, v)
+    μ2, logit_2 = flow_net(net2_params, jnp.concatenate((v, x), axis=1))
     σ2 = nn.sigmoid(logit_2)
     z = z*σ2 + μ2
 
@@ -135,12 +135,16 @@ def build_aux_flow(hps: HyperParams):
 
     return z, v, logdet
   
-  def _reverse_aux_var(zT, vT, params):
-    """Reverse model in auxiliary variable procedure."""
+  def _reverse_aux_var(zx, v, params):
+    """Reverse model in auxiliary variable procedure.
+    
+    Args:
+      `zx` = jnp.concatenate((z, x), axis=1)
+    """
     rv_net_params = params
     
-    mean_vT, logvar_vT = model_net(rv_net_params, zT)
-    logrvT = log_normal(vT, mean_vT, logvar_vT)
-    return logrvT
+    mean_v, logvar_v = model_net(rv_net_params, zx)
+    logrv = log_normal(v, mean_v, logvar_v)
+    return logrv
     
   return init_fun, sample
