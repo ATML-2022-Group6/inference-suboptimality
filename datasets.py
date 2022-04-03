6c7d4e1dd840f55cd75b5e8c13c1109d059cf6f3
Basim Khajwal <basimkhajwal@gmail.com>
2022-04-03 23:30:27 +0100

    Add Fashion Dataset

--- datasets.py
+++ datasets.py
@@ -1,9 +1,11 @@
+import gzip
 import pickle
-import numpy as np
 
+import numpy as np
 from jax import numpy as jnp
 
 MNIST_PATH = "datasets/mnist.pkl"
+FASHION_PATH = "datasets/fashion_{}.gz"
 
 def get_mnist(path = MNIST_PATH):
   """ Load Larochelle (binarised) MNIST data """
@@ -19,9 +21,25 @@ def get_mnist(path = MNIST_PATH):
       mnist[k] = jnp.array(mnist[k])
     return mnist
 
-def get_fashion_mnist():
-  # TODO: Fill this in
-  pass
+def get_fashion_mnist(path = FASHION_PATH):
+
+  def get_dataset(type):
+    with gzip.open(path.format(type + "_labels"), "rb") as f:
+      labels = np.frombuffer(f.read(), dtype=np.uint8, offset=8)
+    with gzip.open(path.format(type + "_images"), "rb") as f:
+      images = np.frombuffer(f.read(), dtype=np.uint8, offset=16)
+      images = images.reshape(len(labels), 784)
+
+    # Binarise images
+    images = np.round(images / 255.)
+
+    # Convert to JAX arrays
+    labels = jnp.array(labels) 
+    images = jnp.array(images)
+
+    return { type+"_x": images, type+"_y": labels }
+
+  return { **get_dataset("train"), **get_dataset("test") }
 
 def get_batches(data, batch_size, smaller_data=False, smaller_size=1000):
   """
